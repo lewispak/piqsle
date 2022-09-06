c0753ced89f556f856101bad15222105b0ac5fb1
Lewis <lewisnpakoti@gmail.com>
2022-09-07 00:00:16 +1200

    centered SimpleGrid with margin=auto

--- src/components/GridRow.js
+++ src/components/GridRow.js
@@ -3,7 +3,7 @@ import { SimpleGrid, Box } from '@chakra-ui/react'
 
 export default function GridBoard() {
     return (
-        <SimpleGrid align="center" width="50%" columns={5} spacing={2} pos="center">
+        <SimpleGrid margin="auto" width="50%" columns={5} spacing={2} pos="center">
             <Box bg='tomato' height='80px'></Box>
             <Box bg='tomato' height='80px'></Box>
             <Box bg='tomato' height='80px'></Box>
