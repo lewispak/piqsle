dde06ad61c3aae5ba342ea10e67e31728a9a610f
Lewis <lewisnpakoti@gmail.com>
2022-09-06 23:51:04 +1200

    added boxes to GridRow to create grid. Still need to be centered

---
+++ src/components/GridRow.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SimpleGrid, Box } from '@chakra-ui/react'
+
+export default function GridBoard() {
+    return (
+        <SimpleGrid align="center" width="50%" columns={5} spacing={2} pos="center">
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+            <Box bg='tomato' height='80px'></Box>
+        </SimpleGrid>
+    )
+}
